ad8ba6d1dc5775838fa34d00b5d0875774a92c1f
hoe-rake <ludoludo@outlook.jp>
2023-10-14 11:46:16 +0900

    :lipstick: add style format

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "広島阿國 | カバディチーム";
+export const SITE_DESCRIPTION =
+  "未経験者の体験希望を随時募集・歓迎しています。阿國は広島市を拠点に活動しているカバディチームです。";
--- src/lib/newt.ts
+++ src/lib/newt.ts
@@ -1,26 +1,26 @@
-import { createClient } from 'newt-client-js'
+import { createClient } from "newt-client-js";
 
 export const newtClient = createClient({
   spaceUid: import.meta.env.NEWT_SPACE_UID,
   token: import.meta.env.NEWT_CDN_API_TOKEN,
-  apiType: 'cdn',
-})
+  apiType: "cdn",
+});
 
 export interface Article {
-  title: string
-  slug: string
-  body: string
+  title: string;
+  slug: string;
+  body: string;
   coverImage: {
-    "_id": string,
-    "src": string,
-    "fileName": string,
-    "fileType": string,
-    "fileSize": number,
-    "width": number,
-    "height": number,
-    "title":string,
-    "altText": string,
-    "description": string,
-    "metadata": {}
-  },
+    _id: string;
+    src: string;
+    fileName: string;
+    fileType: string;
+    fileSize: number;
+    width: number;
+    height: number;
+    title: string;
+    altText: string;
+    description: string;
+    metadata: {};
+  };
 }
