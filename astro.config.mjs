3fa1949f46c9b8ab76ecbf63782ffaf95f655392
hoe-rake <ludoludo@outlook.jp>
2023-10-22 17:28:07 +0900

    :construction: wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import partytown from '@astrojs/partytown';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://agni.ninja',
-  integrations: [sitemap(), tailwind(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), react()]
-});
+	site: 'https://agni.ninja',
+	integrations: [
+		sitemap(),
+		tailwind(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push']
+			}
+		}),
+		react()
+	],
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,7 +15,7 @@ export default {
 			air: '#c4ced6',
 			khaki: '#c3b296',
 			white: '#f6f5f7',
-			blue: '#001f3f'
+			'deep-blue': '#001f3f'
 		},
 		fontFamily: {
 			'zen-antique': ['Zen Antique'],
@@ -37,6 +37,24 @@ export default {
 			},
 			colors: {
 				sandGra: 'linear-gradient(225deg, #d1bea1, #b0a087)'
+			},
+			width: {
+				gold: '61.8%'
+			},
+			keyframes: {
+				aura: {
+					'0%': {
+						background:
+							'linear-gradient(305deg, rgba(216, 50, 50, 1) 0 %, rgba(229, 155, 155, 1) 27 %, rgba(230, 150, 231, 1) 39 %, rgba(244, 184, 72, 1) 48 %, rgba(236, 195, 137, 1) 59 %, rgba(245, 136, 136, 1) 76 %, rgba(240, 243, 240, 1) 88 %, rgba(236, 255, 236, 1) 99 %)'
+					},
+					'100%': {
+						background:
+							'linear-gradient(305deg, rgba(159,37,37,1) 0%, rgba(160,111,111,1) 16%, rgba(154,94,155,1) 31%, rgba(187,160,109,1) 41%, rgba(189,153,87,1) 48%, rgba(165,132,85,1) 55%, rgba(146,74,74,1) 66%, rgba(7,105,7,1) 76%, rgba(62,80,62,1) 99%)'
+					}
+				}
+			},
+			animation: {
+				aura: 'aura 1.2s ease 2s 1 forwards'
 			}
 		}
 	},
