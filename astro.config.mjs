3ece776fa1f6012a2d653d8f96d60bbb54cddb6c
hoe-rake <ludoludo@outlook.jp>
2023-11-08 22:22:03 +0900

    :art: minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import partytown from '@astrojs/partytown';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://agni.ninja',
-	integrations: [
-		sitemap(),
-		tailwind(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push']
-			}
-		}),
-		react()
-	],
-})
+  site: 'https://www.agni.ninja',
+  integrations: [sitemap(), tailwind(), react()]
+});
